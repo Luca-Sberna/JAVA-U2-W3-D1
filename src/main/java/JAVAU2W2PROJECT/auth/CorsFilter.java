f35716fce5f513b490cb81eddbc62b2d23f51dd6
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-13 09:43:18 +0200

    add

---
+++ src/main/java/JAVAU2W2PROJECT/auth/CorsFilter.java
@@ -0,0 +1,22 @@
+package JAVAU2W2PROJECT.auth;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CorsFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+		filterChain.doFilter(request, response);
+	}
+
+}
